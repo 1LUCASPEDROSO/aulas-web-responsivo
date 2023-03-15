56041eda72042d7863772a28950ca497fb89d5ce
lucas pedroso <pedrosolucas1745@gmail.com>
2023-03-15 11:42:49 -0300

    subindo versão organizada em pastas

---
+++ siteRecomendaFilmes/js/index.js
@@ -0,0 +1,5 @@
+//alert('clique no cartaz do filme para ler sua sinopse');
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems);
+  });
