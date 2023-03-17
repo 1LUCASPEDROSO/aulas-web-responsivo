f22da7920d24a4ba4ed7ec01cc160eae661bec33
lucas pedroso <pedrosolucas1745@gmail.com>
2023-03-17 12:02:03 -0300

    subindo versão 1 com js

---
+++ siteRecomendaFilmes/js/controllers/controllerLogin.js
@@ -0,0 +1,6 @@
+  export const dadosLogin = {
+   firstName: document.querySelector('#firstName'),
+   lastName : document.querySelector('#lastName'),
+   firstName :document.querySelector('#email'),
+   firstName : document.querySelector('#password')
+ }
---
+++ siteRecomendaFilmes/js/models/ModelLogin.js
@@ -0,0 +1,10 @@
+ import { dadosLogin } from "../controllers/controllerLogin";
+    let _firstName = dadosLogin.firstName;
+    let _lastName = dadosLogin.lastName;
+    let _email = dadosLogin.email;
+    let _pasword = dadosLogin.password;
+function gerateCadastrer(firstName,lastname,email,password)
+{
+    _firstName.stringfy
+}
+module.exports = ModelLogin;
