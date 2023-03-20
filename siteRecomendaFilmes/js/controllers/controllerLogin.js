0fc2a8d08c10a85bbcd680bafc702dd725ed127a
lucas pedroso <pedrosolucas1745@gmail.com>
2023-03-20 12:12:37 -0300

    subindo verão 3

--- siteRecomendaFilmes/js/controllers/controllerLogin.js
+++ siteRecomendaFilmes/js/controllers/controllerLogin.js
@@ -1,6 +1,18 @@
-export class controllerLogin{
-firstName = document.querySelector('#firstName');
-lastName = document.querySelector('#lastName');
-firstName = document.querySelector('#email');
-firstName = document.querySelector('#password');
+function autenticate()
+{
+ var firstName = document.querySelector('#firstName').value;
+ var lastName = document.querySelector('#lastName').value;
+ var email = document.querySelector('#email').value;
+ var password = document.querySelector('#password').value;
+
+ if(firstName|lastName|email|password == '')
+ {
+    alert('você esqueceu de digitar dados necessários, por favor realize cadastro novamente')
+ }
+ else
+ {
+    dados = {'firstName':firstName,'lastName':lastName,'email':email,'password':password};
+    JSON.stringify(dados);
+    window.open('http://127.0.0.1:5500/alugar.html')
+ }
 }
--- siteRecomendaFilmes/js/index.js
+++ siteRecomendaFilmes/js/index.js
@@ -1,5 +1,4 @@
-//alert('clique no cartaz do filme para ler sua sinopse');
-  document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.modal');
     var instances = M.Modal.init(elems);
   });
