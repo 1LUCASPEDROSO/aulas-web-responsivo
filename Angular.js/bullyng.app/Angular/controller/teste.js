cf92d70c79f22099c6788878e3899efa6c06be3d
lucas pedroso <pedrosolucas1745@gmail.com>
2023-04-11 12:11:33 -0300

    subindo vesão 1.2

--- Angular.js/bullyng.app/Angular/controller/teste.js
+++ Angular.js/bullyng.app/Angular/controller/teste.js
@@ -1,4 +1,13 @@
 var app = angular.module('myApp', []);
 app.controller('myCtrl', function($scope) {
-    $scope.name = "lucas pedroso";
+    $scope.frase = "";
+    if($scope.frase.length > 10)
+    {
+        console.log('teste')
+    }
 });
+const p = document.querySelector('.frase').value;
+if(p.length > 10)
+{
+    console.log('teste');
+}
